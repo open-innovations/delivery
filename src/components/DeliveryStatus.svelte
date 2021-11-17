4b62dbd6d0f11577798f5d3dcff7e249a630c479
Giles Dring <giles@dringtech.com>
2021-11-17 14:39:16 +0000

    Tweaking box hovers

--- src/components/DeliveryStatus.svelte
+++ src/components/DeliveryStatus.svelte
@@ -22,19 +22,22 @@
     padding: 0;
     display: grid;
     grid-template-columns: 1fr 1fr;
-    gap: 0.5em;
+    gap: 15px;
   }
   li {
     list-style-type: none;
     border: 1px solid #ddd;
     border-radius: 0.5em;
-    box-shadow: 0 2px 5px #aaae;
+    box-shadow: 0 7px 5px #aaae;
+    position: relative;
+    top: -5px;
+    transition-property: top, box-shadow;
+    transition-duration: 0.5s;
     overflow: hidden;
   }
   li:hover {
-    box-shadow: 0 4px 5px #aaae;
-    top: -2px;
-    position: relative;
+    box-shadow: 0 2px 5px #aaae;
+    top: 0;
   }
   nav {
     display: flex;
